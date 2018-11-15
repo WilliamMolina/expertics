7c2e35beff27eae95b8bc116f1efa7f42760c2f1
JULIAN HENAO MARIN <julianhenao@SETIAD.COM.CO>
2018-11-15 11:15:08 -0500

    Agregar compose

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/url", (req, res, next) => {
+app.get("/api", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
